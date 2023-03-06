8b5c654f8a68090ad92757ebbd4e88cc8e01ccfc
giansimo <gian_simo94@hotmail.it>
2023-03-06 14:35:51 +0100

    Fix test in Main.java

--- Checkpoint1/src/MainCheckpoint.java
+++ Checkpoint1/src/MainCheckpoint.java
@@ -1,10 +1,10 @@
-public class Main {
+public class MainCheckpoint {
     /*
         * Istanziare 3 oggetti di tipo Libro usando i 2 costruttori creati
         * Istanziare un oggetto di tipo Biblioteca a partire dai libri creati
         * Stampare i libri contenuti nella Biblioteca
         * Confrontare due dei libri creati a scelta
-        * Aggiungere i libri contenuti nella Biblioteca e stampare nuovamente i libri contenuti nella biblioteca per verificarne l’aggiunta
+        * Aggiungere un nuovo libro nella Biblioteca e stampare nuovamente la lista di libri contenuti nella stessa, per verificarne gli elementi aggiornati.
         ** Bonus ** Recuperare tutti i libri di un determinato autore
      */
     public static void main(String[] args) {
